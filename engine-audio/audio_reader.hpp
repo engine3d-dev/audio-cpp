46f18d2c62fb2070d96189a4669e301ad794b3ba
type <type>
2025-02-26 16:34:04 -0800

    Did some renaming and refactoring of the code style

--- engine-audio/audio_reader.hpp
+++ engine-audio/audio_reader.hpp
@@ -2,24 +2,25 @@
 
 #include <span>
 #include <string>
+#include <cstdint>
 
-namespace engine3d::audio{
+namespace audio{
     /**
     * @note Audio Reader takes in any type of audio file.
     * @note Using this API allows you to control how you read in that data.
     * @note Such as ReadInt, ReadAs<T>, ReadFloat, ReadString, etc.
     * 
     */
-    class AudioReader{
+    class audio_reader{
     public:
-        AudioReader(const std::string& p_Filename);
+        audio_reader(const std::string& p_Filename);
 
-        bool IsFileValid() const { return m_IsAudioFileValid; }
+        bool is_valid() const { return m_IsAudioFileValid; }
 
         //! @note We are reading data from a specified audio file
         //! @param p_Filename is the location of the audiofile we are reading from.
         //! @note Basically should allow us to read in those files and play them.
-        static std::span<uint8_t> Read(const std::string& p_Filename);
+        static std::span<uint8_t> read(const std::string& p_Filename);
 
         template<typename T>
         T ReadAs(){
--- src/engine-audio/internal/miniaudio.cpp
+++ src/engine-audio/internal/miniaudio.cpp
@@ -1,3 +1,3 @@
 #define MINIAUDIO_IMPLEMENTATION
 // #include "miniaudio.h"
-#include <engine3d-audio/internal/miniaudio.h>
+#include <engine-audio/internal/miniaudio.h>
