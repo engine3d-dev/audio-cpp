3e9f3012856202a223bee4f3c42748841f24fd67
= <=>
2025-09-27 14:49:45 -0700

    Reformatted audio_context.hpp

--- audio-cpp/drivers/audio_context.hpp
+++ audio-cpp/drivers/audio_context.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
-
 namespace audio {
     /**
      * @brief Low-level audio context for setting up miniaudio
-    */
+     */
     class audio_context {
     public:
         audio_context() = default;
